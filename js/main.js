9c9708b1a14c8be653db0a10ded5d73d4a0ff863
Вера Левина <lyovinka@gmail.com>
2023-03-21 23:35:24 +0300

    Исправление 7 раздела 1.1.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import { getPhotoArray } from '/.data.js';
-import { generateThumbnails } from '/.thumbnailrendering.js';
+import { getPhotoArray } from './data.js';
+import { generateThumbnails } from './thumbnailrendering.js';
 generateThumbnails(getPhotoArray());
